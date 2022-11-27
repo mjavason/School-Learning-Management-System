18b6f0a2bbbfc356b33e9edf0afa6d45a2706fe8
emerald2240 <orjimichael4886@gmail.com>
2022-11-27 20:29:13 +0100

    changed site logo alt

--- user/includes/header.php
+++ user/includes/header.php
@@ -6,7 +6,7 @@
 					<div class="header-row">
 						<div class="header-logo">
 							<a href="index">
-								<img alt="Porto" width="100" height="48" data-sticky-width="82" data-sticky-height="40" src="img/logo-default-slim.png">
+								<img alt="ESUT Repo" width="100" height="48" data-sticky-width="82" data-sticky-height="40" src="img/logo-default-slim.png">
 							</a>
 						</div>
 					</div>
